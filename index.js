8ec071e46dfb275340fde2e459f1be567bdf09e7
DidrikF <didrik.fleischer@gmail.com>
2018-10-05 14:07:18 +0200

    index.js added

---
+++ index.js
@@ -0,0 +1,11 @@
+// Code tcp client server code
+
+// Code command line interface
+
+// Code diffie-hellman algorithm 
+
+// Code PRNG (RC4 i think)
+
+// Setup AES communication between the two parties
+
+// Enable sending of messages
